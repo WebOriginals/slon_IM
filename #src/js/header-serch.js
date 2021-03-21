f954f80eea551b226b45919c16a1a34e880a00d6
WebOriginals <web-originals@yandex.ru>
2021-03-21 21:14:54 +0300

    анимация для поиска

--- #src/js/header-serch.js
+++ #src/js/header-serch.js
@@ -3,12 +3,18 @@ if( $( '.ic-btn__search' ).length ) {
         $(this).toggleClass("active");
         if (this.classList.contains("active") === true) {
             $('.body-header__bottom').addClass("body-header__bottom-open");
+            $('.header').addClass("cust");
+            $('body').addClass("mask");
         } else {
             $('.body-header__bottom').removeClass("body-header__bottom-open");
+            $('body').removeClass("mask");
+            $('.header').removeClass("cust");
         }
     });
     $(".wrapper-search-header__close").click(function () {
         $('.search-header').removeClass("active");
         $('.body-header__bottom').removeClass("body-header__bottom-open");
+        $('body').removeClass("mask");
+        $('.header').removeClass("cust");
     });
 }
--- dist/js/script.js
+++ dist/js/script.js
@@ -919,13 +919,19 @@ if( $( '.filter-slider-price' ).length ) {
         $(this).toggleClass("active");
         if (this.classList.contains("active") === true) {
             $('.body-header__bottom').addClass("body-header__bottom-open");
+            $('.header').addClass("cust");
+            $('body').addClass("mask");
         } else {
             $('.body-header__bottom').removeClass("body-header__bottom-open");
+            $('body').removeClass("mask");
+            $('.header').removeClass("cust");
         }
     });
     $(".wrapper-search-header__close").click(function () {
         $('.search-header').removeClass("active");
         $('.body-header__bottom').removeClass("body-header__bottom-open");
+        $('body').removeClass("mask");
+        $('.header').removeClass("cust");
     });
 }
 
